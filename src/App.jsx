ece3b4e35317a8c268ade0cdb0da07c2d767779e
SpamNidsiwa <james33274@hotmail.com>
2020-03-24 15:57:32 +0700

    :D

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <Router basename = "/lab8-expenses">
+      <Router basename = "/lab08cpe">
       <div className="container mt-4 mb-5">
         <Header />
 
